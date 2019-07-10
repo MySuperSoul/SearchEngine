beacc124de2f0185fbd1ecdd56f10d078edcf5d7
huangyifei <1144358492@qq.com>
2019-07-10 23:54:41 +0800

    update utils class method for api

--- crawler/CrawlerUtils/Utils.py
+++ crawler/CrawlerUtils/Utils.py
@@ -5,16 +5,24 @@ from lxml import etree
 class Utils():
     @classmethod
     def GetPageContent(cls, token, url):
-        fields = ','.join(['text', ])
-        params = {
-            'token': token,
-            'url': url,
-            'fields': fields
-        }
+        retry_time = 5
+        while retry_time >= 0:
+            fields = ','.join(['text', ])
+            params = {
+                'token': token,
+                'url': url,
+                'fields': fields
+            }
 
-        result = requests.get('http://api.url2io.com/article', params=params)
-        content = result.json()
-        return content
+            result = requests.get('http://api.url2io.com/article', params=params)
+            if result.status_code == 200:
+                content = result.json()
+                return content
+            else:
+                retry_time -= 1
+        else:
+            raise Exception('api error')
+
 
     @classmethod
     def GetResponseForJson(cls, content):

4bbff1294a060485f1d1bbe6473721ae69fe2213
huangyifei <1144358492@qq.com>
2019-07-11 17:31:39 +0800

    fix jianshu page error

--- crawler/JianShuCrawler/demo_spider/settings.py
+++ crawler/JianShuCrawler/demo_spider/settings.py
@@ -22,7 +22,7 @@ NEWSPIDER_MODULE = 'demo_spider.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 300
+CONCURRENT_REQUESTS = 100
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay

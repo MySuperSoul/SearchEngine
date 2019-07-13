8954d4851d010491105f2a60766d4352b497cf26
huangyifei <1144358492@qq.com>
2019-07-13 23:46:06 +0800

    update post-processors, complete zhihu processing

--- post-processors/zhihu/AutoSummaryHandler.py
+++ post-processors/zhihu/AutoSummaryHandler.py
@@ -1,6 +1,11 @@
+from textrank4zh import TextRank4Sentence
+
 class AutoSummaryHandler():
     def __init__(self):
-        pass
+        self.tr4w = TextRank4Sentence()
 
     def GetSummary(self, content) -> str:
-        return '1'
+        self.tr4w.analyze(text=content, lower=True, source='all_filters')
+        key_sentences = self.tr4w.get_key_sentences(num=2)
+        summary = '. '.join(sentence['sentence'] for sentence in key_sentences)
+        return summary
--- post-processors/zhihu/main.py
+++ post-processors/zhihu/main.py
@@ -1,6 +1,40 @@
 from common.DataFilterHandler import DataFilterHandler
+from common.Config import MongoConfig
+from zhihu.AutoSummaryHandler import AutoSummaryHandler
+from common.AutoTransferHandler import AutoTransferHandler
+
+def Summary():
+    config = MongoConfig(database_name='TechHub', collection_name='zhihu', use_localhost=False)
+    cursor = config.GetMongoCursor()
+    summary_handler = AutoSummaryHandler()
+    count = 0
+
+    for document in cursor.find():
+        content = document['content']
+        url = document['url']
+        if len(content) < 10:
+            cursor.delete_one({"url": url})
+            print('Remove too short document')
+        else:
+            summary = summary_handler.GetSummary(content=content)
+            cursor.update_one({'url': url}, {'$set': {'summary': summary}})
+            print('Finish summary for document {}'.format(count))
+        count += 1
+
+    # release resources
+    config.CloseConnection()
+
+def Transfer():
+    handler = AutoTransferHandler(database_name='TechHub', collection_name_from='zhihu', collection_name_to='infos')
+    handler.start()
 
 if __name__ == '__main__':
     # first filter the data
-    filter = DataFilterHandler(database_name='TechHub', collection_name='zhihu', use_localhost=False)
-    filter.start()
+    # filter = DataFilterHandler(database_name='TechHub', collection_name='zhihu', use_localhost=False)
+    # filter.start()
+
+    # Then auto summary the content
+    # Summary()
+
+    # Transfer data
+    Transfer()

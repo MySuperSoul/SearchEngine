e25a09bf405fdf90f64dfa9bdfdb21c0ce24f823
Chen Xin <1275234126@qq.com>
2019-07-14 14:14:30 +0800

    更换端口

--- back-end/docker-run.sh
+++ back-end/docker-run.sh
@@ -2,7 +2,7 @@
 set -u
 
 # service-register
-docker run -id --rm --name main --net=host -p 7000:7000 -t main --spring.profiles.active=dev
+docker run -id --rm --name main --net=host -p 9999:9999 -t main --spring.profiles.active=dev
 
 
 

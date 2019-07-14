9fa0002519d162796aa947719936b77e05fc05dc
Chen Xin <1275234126@qq.com>
2019-07-14 14:51:46 +0800

    增加API文档，增加Bilibili和CSDN的时间处理（初步）

---
+++ post-processors/CSDN/TimeProcess.py
@@ -0,0 +1,15 @@
+from common.Config import MongoConfig
+
+if __name__ == '__main__':
+    config = MongoConfig(database_name='TechHub', collection_name='CSDN')
+    cursor = config.GetMongoCursor()
+    count = 0
+
+    for document in cursor.find():
+        url = document['url']
+        new_time = document['date'].split()[0]
+        cursor.update_one({'url' : url}, {'$set' : {'date' : new_time}})
+        count += 1
+        print('Finish update document {}'.format(count))
+
+    config.CloseConnection()
